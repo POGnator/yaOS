98213d96aadc53c28fac90723534f36c7898a376
POGnator <97893135+POGnator@users.noreply.github.com>
2022-12-29 20:35:16 +0100

    explain myself at start

--- kernel.c
+++ kernel.c
@@ -1,3 +1,9 @@
+/*
+  This file is compiled but not used.
+  I have the hopes that C++ will make a lot things here easier.
+*/
+
+
 #include "terminal/std-io.h"
 
 void kernel_entry()
@@ -5,15 +11,15 @@ void kernel_entry()
   //max size of input capped to 100
   char arr[100];
   init_vga(WHITE, GREEN);
-  printstr("yaOS booted\n");
+  printstr((char*)"yaOS with C++ booted\n");
   //Puts the system into an endless loop.
-  while(strcmp(arr, "CLOSE") == 0){
-    printstr("yaOS> ");
+  while(strcmp(arr, (char*)"CLOSE") == 0){
+    printstr((char*)"yaOS> ");
     input(arr);
     //print_char('\n');
     //First try of a command handler, subject to change.
-    if(strcmp(arr, "PROGRAM")==1){
-      printstr("Will print ToDo list once finished.\n");
+    if(strcmp(arr, (char*)"PROGRAM")==1){
+      printstr((char*)"Will print ToDo list once finished.\n");
     }
     print_char('\n');
     print_int(vga_index);
